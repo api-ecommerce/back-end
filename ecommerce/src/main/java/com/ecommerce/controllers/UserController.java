4995f1a784af1440e6c8ed29a90a66e18e519a85
Guilherme Cavalcante <guicavalcante59@gmail.com>
2024-08-22 00:20:07 -0300

    feat: endpoint de Hello World

---
+++ ecommerce/src/main/java/com/ecommerce/controllers/UserController.java
@@ -0,0 +1,13 @@
+package com.ecommerce.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+
+
+@Controller
+public class UserController {
+
+    public ResponseEntity helloWorld(){
+        return ResponseEntity.ok("Hello World");
+    }
+}
